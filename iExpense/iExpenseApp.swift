2f5d6fd6c3350584ea8b05f5794af9559c376a37
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-09 17:47:28 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dechon Ryan on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
