14052ec1695e17bee5430cd89e2953c224412fc6
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-10 17:42:00 -0700

    Create Struct for ExpenseItem

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Dechon Ryan on 5/10/24.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
---
+++ iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  iExpense
+//
+//  Created by Dechon Ryan on 5/10/24.
+//
+
+import Foundation
