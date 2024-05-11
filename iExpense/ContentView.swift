3f1b8e55afc72db6b3bcdb8293c03792470b3220
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-10 17:51:36 -0700

    Create basic layout; Enable item deletion

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            // more to come
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
